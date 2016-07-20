68fd4a705e8639a392ec01ebfefc113a0df54248
Vasiliy Uchanev <Vasily@sharp-dev.net>
2016-07-20 18:18:41 +0500

    phoneword logic

--- HelloWorld/MainActivity.cs
+++ HelloWorld/MainActivity.cs
@@ -1,30 +1,56 @@
 ﻿using System;
 using Android.App;
 using Android.Content;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Android.Widget;
+using Uri = Android.Net.Uri;
 
 namespace HelloWorld
 {
-	[Activity(Label = "HelloWorld", MainLauncher = true, Icon = "@drawable/icon")]
+	[Activity(Label = "Phone Word", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
-
 		protected override void OnCreate(Bundle bundle)
 		{
 			base.OnCreate(bundle);
-
-			// Set our view from the "main" layout resource
+			
 			SetContentView(Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button>(Resource.Id.MyButton);
+			var phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+			var translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+			var callButton = FindViewById<Button>(Resource.Id.CallButton);
 
-			button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+			callButton.Enabled = false;
+			var translatedNumber = String.Empty;
+
+			translateButton.Click += (sender, args) =>
+			{
+				translatedNumber = PhonewordTranslator.ToNumber(phoneNumberText.Text);
+				if (string.IsNullOrWhiteSpace(translatedNumber))
+				{
+					callButton.Text = "Call";
+					callButton.Enabled = false;
+				}
+				else
+				{
+					callButton.Text = "Call " + translatedNumber;
+					callButton.Enabled = true;
+				}
+			};
+
+			callButton.Click += (sender, args) =>
+			{
+				var callDialog = new AlertDialog.Builder(this);
+				callDialog.SetMessage("Call " + translatedNumber + "?");
+				callDialog.SetNeutralButton("Call", delegate
+				{
+					var callIntent = new Intent(Intent.ActionCall);
+					callIntent.SetData(Uri.Parse("tel:" + translatedNumber));
+					StartActivity(callIntent);
+				});
+				callDialog.SetNegativeButton("Cancel", delegate { });
+				callDialog.Show();
+			};
 		}
 	}
 }
